568f4ba7276d86aca474e36498ecdbf6aeac7c41
RubinaKariyadan <167404198+RubinaKariyadan@users.noreply.github.com>
2025-12-30 22:25:55 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("Space Mission Recommendation System")
+st.write("App loaded successfully")
 st.write("Data-Driven Decision Support for Space Mission Planning")
