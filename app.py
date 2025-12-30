24b27a1e844cb91549a6c22d6c6993d44712bda0
RubinaKariyadan <167404198+RubinaKariyadan@users.noreply.github.com>
2025-12-30 22:19:03 +0400

    Update and rename app (1).py to app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
-!streamlit run app.py --server.port 8501
+import streamlit as st
+import pandas as pd
+import numpy as np
 st.title("Space Mission Recommendation System")
 st.write("Data-Driven Decision Support for Space Mission Planning")
