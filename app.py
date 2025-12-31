741e3144792481a3e13f90bc01fe0033616ea71b
RubinaKariyadan <167404198+RubinaKariyadan@users.noreply.github.com>
2025-12-31 21:46:33 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("Space Mission Recommendation System")
-df = pd.read_csv("space_mission_dataset.csv")
+df = pd.read_csv("space_missions_dataset.csv")
 
 st.sidebar.header("Mission Preferences")
 budget = st.sidebar.slider("Budget (Billion USD)", 10, 500, 100)
