f18580cec976ece8b8d96c52550438ce5e575f40
RubinaKariyadan <167404198+RubinaKariyadan@users.noreply.github.com>
2025-12-31 22:03:15 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title("Space Mission Recommendation System")
 df = pd.read_csv("space_missions_dataset.csv")
+st.dataframe(df.head())
+X = df[["Scientific_ROI", "Mission Cost (billion USD)", "Mission Success (%)"]]
+kmeans = KMeans(n_clusters=3, random_state=42)
+df["Cluster"] = kmeans.fit_predict(X)
+st.title("Space Mission Recommendation System")
 
 st.sidebar.header("Mission Preferences")
 budget = st.sidebar.slider("Budget (Billion USD)", 10, 500, 100)
 risk = st.sidebar.selectbox("Risk Tolerance", ["Low", "Medium", "High"])
 
 st.subheader("Dataset Overview")
-st.dataframe(df.head())
+
 recommendation = None
 if st.button("Recommend Mission"):
     recommendation = recommend_mission(budget, risk)
